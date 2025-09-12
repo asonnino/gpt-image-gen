ef0cdf135065f961e7a8894d87af41434bbd4670
Alberto Sonnino <alberto@sonnino.com>
2025-09-12 18:15:54 +0900

    Fix CI: Update minimum Python version to 3.8
    - Python 3.7 is not available on Ubuntu 24.04 runners
    - Update CI to test Python 3.8-3.12
    - Update setup.py to require Python 3.8+
    - Update README badge to show Python 3.8+

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=requirements,
     entry_points={
         "console_scripts": [
